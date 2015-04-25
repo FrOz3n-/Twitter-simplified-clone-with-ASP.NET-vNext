1b187c9de4606643b8a10b00e2a4264580ff517e
ice jovanoski <ice.jovanoski@haselt.com>
2015-04-25 12:54:34 +0200

    Simple to-do CRUD app to get started with vNext.

---
+++ src/nexTwitter/Services/StatisticsService.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using nexTwitter.Models;
+
+namespace nexTwitter.Services
+{
+  public class StatisticsService
+  {
+    private readonly ApplicationDbContext db;
+
+    public StatisticsService(ApplicationDbContext context)
+    {
+      db = context;
+    }
+
+    public async Task<int> GetCount()
+    {
+      return await Task.FromResult(db.TodoItems.Count());
+    }
+
+    public async Task<int> GetCompletedCount()
+    {
+      return await Task.FromResult(
+          db.TodoItems.Count(x => x.IsDone == true));
+    }
+
+    public async Task<double> GetAveragePriority()
+    {
+      return await Task.FromResult(
+          db.TodoItems.Average(x =>
+                     (double?)x.Priority) ?? 0.0);
+    }
+  }
+}
